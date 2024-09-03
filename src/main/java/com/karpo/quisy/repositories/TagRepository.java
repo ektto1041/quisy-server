a9571cfc19dbaae17d383245ca043153f5f4ea4c
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-09-03 12:20:11 +0900

    feat: findByWorkbookId Query 추가

--- src/main/java/com/karpo/quisy/repositories/TagRepository.java
+++ src/main/java/com/karpo/quisy/repositories/TagRepository.java
@@ -1,9 +1,21 @@
 package com.karpo.quisy.repositories;
 
+import com.karpo.quisy.dtos.TagWithWorkbookIdDto;
 import com.karpo.quisy.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    @Query(
+        value = "SELECT new com.karpo.quisy.dtos.TagWithWorkbookIdDto(w.workbookId, t.name) " +
+        "FROM WorkbookTag wt " +
+        "JOIN wt.workbook w " +
+        "JOIN wt.tag t " +
+        "WHERE w.workbookId IN (:workbookIds)"
+    )
+    List<TagWithWorkbookIdDto> findByWorkbookId(List<Long> workbookIds);
 }
