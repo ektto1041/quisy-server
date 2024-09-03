4756aee78e8eb2fd21003ea379a11201ace610b6
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-09-03 11:06:42 +0900

    feat: getAllWorkbookPreviews Query 추가

--- src/main/java/com/karpo/quisy/repositories/WorkbookRepository.java
+++ src/main/java/com/karpo/quisy/repositories/WorkbookRepository.java
@@ -1,9 +1,19 @@
 package com.karpo.quisy.repositories;
 
+import com.karpo.quisy.dtos.WorkbookPreviewDto;
 import com.karpo.quisy.entities.Workbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WorkbookRepository extends JpaRepository<Workbook, Long> {
+    @Query("SELECT new com.karpo.quisy.dtos.WorkbookPreviewDto(w.workbookId, u.userId, u.nickname, u.profileImage, w.title, w.description, w.createdAt, w.updatedAt) " +
+            "FROM Workbook w " +
+            "JOIN w.user u")
+    List<WorkbookPreviewDto> getAllWorkbookPreviews();
 }
+
+
